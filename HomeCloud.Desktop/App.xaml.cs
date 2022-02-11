1c0856f4e57838874712442b91e01d6be848f7d1
Aspirio <soultan_98@hotmail.com>
2022-02-11 22:49:55 +0100

    feat: implemented base WPF

--- HomeCloud.Desktop/App.xaml.cs
+++ HomeCloud.Desktop/App.xaml.cs
@@ -13,5 +13,9 @@ namespace HomeCloud.Desktop
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
     }
 }
---
+++ HomeCloud.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCloud.Desktop.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
