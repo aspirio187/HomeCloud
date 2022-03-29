5b9087fca5dd19b890b3a3ac1c9f8fd8c66a3a3a
Aspirio <soultan_98@hotmail.com>
2022-03-29 16:37:22 +0200

    feat: implemented FilesManager and created new project Desktop.BLL

--- HomeCloud.Desktop/Managers/FilesManager.cs
+++ HomeCloud.Desktop/Managers/FilesManager.cs
@@ -7,25 +7,44 @@ using System.Threading.Tasks;
 
 namespace HomeCloud.Desktop.Managers
 {
+    /// <summary>
+    /// Manager that take care of synching files on which an action happened with the server
+    /// </summary>
     public class FilesManager
     {
-        public FilesIterator FilesToLoad { get; set; } = new FilesIterator();
+        /// <summary>
+        /// The enumerable in which every changes are loaded
+        /// </summary>
+        public FilesIterator UploadFiles { get; set; } = new FilesIterator();
 
+        /// <summary>
+        /// The task that is going to be runned after an element is added in <see cref="UploadFiles"/>
+        /// </summary>
         public Task FilesSync { get; private set; }
 
+        /// <summary>
+        /// Create <see cref="FilesManager"/> object
+        /// </summary>
         public FilesManager()
         {
-            FilesToLoad.OnElementAdded += FileAdded;
-            FilesToLoad.OnElementDeleted += FileDeleted;
+            UploadFiles.OnElementAdded += FileAdded;
+            UploadFiles.OnElementDeleted += FileDeleted;
 
             FilesSync = new Task(SyncFile);
         }
 
+        /// <summary>
+        /// Method invoked when an element is deleted from <see cref="UploadFiles"/>
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
         private void FileDeleted()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Method run when an element is added in <see cref="UploadFiles"/>
+        /// </summary>
         private void FileAdded()
         {
             if (FilesSync.Status != TaskStatus.Running)
@@ -34,12 +53,16 @@ namespace HomeCloud.Desktop.Managers
             }
         }
 
+        /// <summary>
+        /// <see cref="FilesSync"/>'s action run when an element is added in <see cref="UploadFiles"/>
+        /// </summary>
         public void SyncFile()
         {
             int i = 0;
-            while(FilesToLoad.Length != 0)
+            while (UploadFiles.Length != 0)
             {
-
+                // TODO : Instructions to send the file to the server
+                UploadFiles.RemoveAt(i);
             }
         }
     }
